4efa5d154dcc18608bc5e484d6d98f9f73bcfeb4
Alex Yancey <alexyancey3@gmail.com>
2015-04-30 19:34:51 -0700

    If I don't commit soon I'm gonna be screwed

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import pythoncom, pyHook
 
+FadeFocusKey = "Win+Oem_2"
+WinDown = False
+
+
 # http://sourceforge.net/p/pyhook/wiki/PyHook_Tutorial/
 def OnKeyboardEvent(event):
-    print 'MessageName:',event.MessageName
-    print 'Message:',event.Message
-    print 'Time:',event.Time
-    print 'Window:',event.Window
-    print 'WindowName:',event.WindowName
-    print 'Ascii:', event.Ascii, chr(event.Ascii)
-    print 'Key:', event.Key
-    print 'KeyID:', event.KeyID
-    print 'ScanCode:', event.ScanCode
-    print 'Extended:', event.Extended
-    print 'Injected:', event.Injected
-    print 'Alt', event.Alt
-    print 'Transition', event.Transition
-    print '---'
+	global WinDown
+	global FadeFocusKey
+	HotKey = FadeFocusKey
+	# print 'MessageName:',event.MessageName
+	# print 'Message:',event.Message
+	# print 'Time:',event.Time
+	# print 'Window:',event.Window
+	# print 'WindowName:',event.WindowName
+	# print 'Ascii:', event.Ascii, chr(event.Ascii)
+	# print 'Key:', event.Key
+	# print 'KeyID:', event.KeyID
+	# print 'ScanCode:', event.ScanCode
+	# print 'Extended:', event                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      .Extended
+	# print 'Injected:', event.Injected
+	# print 'Alt', event.Alt 
+	# print 'Transition', event.Transition
+	# print '---'
 
-# return True to pass the event to other handlers
-    return True
+	if event.Key == 'Lwin' or event.Key == 'Rwin':
+		if event.MessageName == 'key down':
+			WinDown = True
+		else:
+			WinDown = False
 
-# create a hook manager
+	print WinDown
+	if WinDown:
+		HotKey = FadeFocusKey.replace("Win+","")
+
+	if HotKey == event.Key:
+		print 'KEY PRESSED'
+	return True
+
+
 hm = pyHook.HookManager()
-# watch for all mouse events
 hm.KeyDown = OnKeyboardEvent
-# set the hook
+hm.KeyUp = OnKeyboardEvent
 hm.HookKeyboard()
-# wait forever
+
+# Get hooked kid
 pythoncom.PumpMessages()
